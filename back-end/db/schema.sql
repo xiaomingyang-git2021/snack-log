3151c5a68dad69bbb4d2a22dd327313e0711c059
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-02-19 14:48:34 -0500

    modified a file

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -3,4 +3,12 @@ CREATE DATABASE snack_a_log;
 
 \c snack_a_log; 
 
-
+CREATE TABLE snacks (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  image TEXT,
+  fiber INTEGER,
+  protein INTEGER,
+  added_sugar INTEGER,
+  is_healthy BOOLEAN
+);
