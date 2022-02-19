abeb604669a8def1f25c93dd4401007974aa7dac
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-02-19 17:02:47 -0500

    added a new file SanckDetails.js

---
+++ front-end/src/Components/SnackDetails.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import HeartHealth from "./HeartHealth";
+
+
+const API = process.env.REACT_APP_API_URL;
+
+function SnackDetails() {
+  const [snack, setSnack] = useState({});
+  let { id } = useParams();
+  let navigate = useNavigate();
+
+  useEffect(()=>{
+    axios.get(API + "/snacks/" + id)
+      .then((res)=>{
+        setSnack(res.data.payload);
+      }).catch((err)=>{
+        console.log(err);
+      })
+  }, [id, API]);
+
+  const handleDelete=()=>{
+    axios.delete(API + "/snacks/" + id)
+      .then((res)=>{
+        navigate("/snacks")
+      }).catch((err)=>{
+        console.log(err);
+      })
+  }
+
+  return <article>
+    <aside>
+    <h4>the snack health</h4>
+    <HeartHealth snackHealth={snack.is_healthy}/>
+    </aside>
+    <div className="Show">
+      <div >
+        <h5>{snack.name}</h5>
+        <img src={snack.image} alt={snack.name}/>
+        <h6>Protein: {snack.protein}</h6>
+        <h6>Fiber: {snack.fiber}</h6>
+        <h6>Added Sugar: {snack.added_sugar}</h6>
+      </div>
+      <div className="showNavigation">
+        <div>
+          <Link to="/snacks">
+            <button>Back</button>
+          </Link>
+        </div>
+        <div>
+          <Link to={`/snacks/${id}/edit`}>
+            <button>Edit</button>
+          </Link>
+        </div>
+        <div>
+          <button onClick={handleDelete}>Delete</button>
+        </div>
+      </div>
+    </div>
+  </article>
+}
+
+export default SnackDetails;
