777590ef5e241068173bb7e7a8cbc011219faca2
Xiaoming Yang <yangxiaoming295@gmail.com>
2022-02-19 12:32:39 -0500

    modified app.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,11 +1,15 @@
 // DEPENDENCIES
-
+const cors = require('cors');
 const express = require("express");
+const snackController = require('./controllers/snackController.js')
 
 // CONFIGURATION
 const app = express();
 
 // MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+app.use('/snacks', snackController);
 
 // ROUTES
 
